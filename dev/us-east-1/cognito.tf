99738b20b31333f39d9ff8f55dafd413d1dc860a
Brandon Lau <brandonlau2@gmail.com>
2020-12-03 14:39:46 -0800

    build: define Cognito resource

---
+++ dev/us-east-1/cognito.tf
@@ -0,0 +1,38 @@
+resource "aws_cognito_user_pool" "default" {
+  name              = "onguard-user-pool"
+  auto_verified_attributes = ["email"]
+  username_attributes = ["email"]
+  mfa_configuration = "OPTIONAL"
+
+  software_token_mfa_configuration {
+    enabled = true
+  }
+
+  password_policy {
+    minimum_length = 8
+    require_symbols = false
+  }
+}
+
+resource "aws_cognito_user_pool_domain" "default" {
+  user_pool_id = aws_cognito_user_pool.default.id
+  domain = "onguard"
+}
+
+resource "aws_cognito_identity_provider" "okta" {
+  user_pool_id  = aws_cognito_user_pool.default.id
+  provider_name = "Okta"
+  provider_type = "SAML"
+
+  provider_details = {
+
+  }
+
+  attribute_mapping = {
+    email = "email"
+    family_name = "lastName"
+    given_name = "firstName"
+  }
+
+}
+
