384cc2b58aee94833bdb684197c8f3461184dcc2
Dennis Lau <dennislzf@gmail.com>
2020-12-03 09:36:34 -0800

    added private route

--- dev/us-east-1/vpc.tf
+++ dev/us-east-1/vpc.tf
@@ -71,11 +71,40 @@ resource "aws_route_table_association" "public_sub_associate_2" {
   route_table_id = aws_default_route_table.default_route_table.id
 }
 
+resource "aws_route_table_association" "private_sub_associate_1" {
+  subnet_id      = aws_subnet.private1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_sub_associate_2" {
+  subnet_id      = aws_subnet.private2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_sub_associate_3" {
+  subnet_id      = aws_subnet.private3.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_sub_associate_4" {
+  subnet_id      = aws_subnet.private4.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
 # Route Table Definitions
 resource "aws_default_route_table" "default_route_table" {
   default_route_table_id = "rtb-72adf30c"
 
   tags = {
     Name = "default table"
+    type = "public"
+  }
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.default.id
+
+  tags = {
+    type = "private"
   }
 }
