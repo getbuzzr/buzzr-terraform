9b5d806706d996acc4d910db59fc293391e49579
Brandon Lau <brandonlau2@gmail.com>
2020-12-03 15:01:31 -0800

    build: add SAML metadata URL

--- dev/us-east-1/cognito.tf
+++ dev/us-east-1/cognito.tf
@@ -1,22 +1,22 @@
 resource "aws_cognito_user_pool" "default" {
-  name              = "onguard-user-pool"
-  auto_verified_attributes = ["email"]
-  username_attributes = ["email"]
-  mfa_configuration = "OPTIONAL"
+  name                      = "onguard-user-pool"
+  auto_verified_attributes  = ["email"]
+  username_attributes       = ["email"]
+  mfa_configuration         = "OPTIONAL"
 
   software_token_mfa_configuration {
     enabled = true
   }
 
   password_policy {
-    minimum_length = 8
+    minimum_length  = 8
     require_symbols = false
   }
 }
 
 resource "aws_cognito_user_pool_domain" "default" {
-  user_pool_id = aws_cognito_user_pool.default.id
-  domain = "onguard"
+  user_pool_id  = aws_cognito_user_pool.default.id
+  domain        = "onguard-dev"
 }
 
 resource "aws_cognito_identity_provider" "okta" {
@@ -24,14 +24,14 @@ resource "aws_cognito_identity_provider" "okta" {
   provider_name = "Okta"
   provider_type = "SAML"
 
-  provider_details = {
-
+  attribute_mapping = {
+    email       = "email"
+    family_name = "lastName"
+    given_name  = "firstName"
   }
 
-  attribute_mapping = {
-    email = "email"
-    family_name = "lastName"
-    given_name = "firstName"
+  provider_details = {
+    MetadataURL = "https://dev-4181175.okta.com/app/exk1lkocfKhH8ytxx5d6/sso/saml/metadata"
   }
 
 }
