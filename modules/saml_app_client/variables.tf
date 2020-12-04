c9ef0ce1f166295630978384507523c4769e43e1
Dennis Lau <dennislzf@gmail.com>
2020-12-04 14:58:14 -0800

    added changes to modules:

---
+++ modules/saml_app_client/variables.tf
@@ -0,0 +1,29 @@
+variable "user_pool_id" {
+  type        = string
+  description = "(required)This is the id of the user pool"
+}
+
+variable "callback_urls" {
+  type        = list(string)
+  description = "This is the list of callback urls allowed"
+  default     = []
+}
+
+variable "default_redirect_uri" {
+  type        = string
+  description = "This is the default reirect uri"
+  default     = ""
+}
+
+variable "logout_urls" {
+  type        = list(string)
+  description = "This is a list of logout urls allowed"
+  default     = []
+}
+
+variable "app_client_name" {
+  type        = string
+  description = "This is the app client name"
+}
+
+
