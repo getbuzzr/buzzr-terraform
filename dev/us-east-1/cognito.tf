98228f6320707c774c5f64a039d3a8730521164f
Dennis Lau <dennislzf@gmail.com>
2020-12-04 14:54:37 -0800

    added windows

--- dev/us-east-1/cognito.tf
+++ dev/us-east-1/cognito.tf
@@ -21,25 +21,47 @@ resource "aws_cognito_user_pool_domain" "default" {
 
 
 module "okta_app_client" {
-  source       = "../../modules/okta_app_client"
-  user_pool_id = aws_cognito_user_pool.default.id
+  source          = "../../modules/saml_app_client"
+  user_pool_id    = aws_cognito_user_pool.default.id
+  app_client_name = "okta_app_client"
   #default_redirect_uri
   default_redirect_uri = ""
   # add callback urls here
   callback_urls = ["https://google.ca"]
   # signout urls
   logout_urls = []
+}
 
+module "adfs_app_client" {
+  source          = "../../modules/saml_app_client"
+  user_pool_id    = aws_cognito_user_pool.default.id
+  app_client_name = "adfs_app_client"
+  #default_redirect_uri
+  default_redirect_uri = ""
+  # add callback urls here
+  callback_urls = ["https://google.ca"]
+  # signout urls
+  logout_urls = []
 }
 
 
 module "okta_dev_identity_provider" {
-  source = "../../modules/okta_identity_provider"
+  source = "../../modules/identity_provider"
   # mapping attributes used to handle attributes returned by saml
   attribute_mapping = {}
+  provider_name     = "okta_dev"
+  user_pool_id      = aws_cognito_user_pool.default.id
+  metadata_url      = "https://dev-4181175.okta.com/app/exk1lkocfKhH8ytxx5d6/sso/saml/metadata"
 
-  user_pool_id = aws_cognito_user_pool.default.id
-  metadata_url = "https://dev-4181175.okta.com/app/exk1lkocfKhH8ytxx5d6/sso/saml/metadata"
+}
+
+module "adfs_dev_identity_provider" {
+  source = "../../modules/identity_provider"
+  # mapping attributes used to handle attributes returned by saml
+  attribute_mapping = {}
+  provider_name     = "adfs_dev"
+  user_pool_id      = aws_cognito_user_pool.default.id
+  metadata_url      = "https://s3-hosted-adfs-xml.com"
 
 }
 
--- modules/okta_app_client/output.tf
+++
@@ -1,3 +0,0 @@
-output "appclient_id" {
-  value = aws_cognito_user_pool_client.okta.id
-}
