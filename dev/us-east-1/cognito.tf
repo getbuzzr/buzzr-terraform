6fc8fdc73bec9a36cae44cef19667bc1088f500b
Dennis Lau <dennislzf@gmail.com>
2020-12-04 15:46:27 -0800

    change url

--- dev/us-east-1/cognito.tf
+++ dev/us-east-1/cognito.tf
@@ -8,12 +8,6 @@ resource "aws_cognito_user_pool" "default" {
   software_token_mfa_configuration {
     enabled = true
   }
-  
-  schema {
-    attribute_data_type = "String"
-    name                = "email"
-    required            = true
-  }
 
   password_policy {
     minimum_length  = 8
@@ -67,7 +61,7 @@ module "adfs_dev_identity_provider" {
   attribute_mapping = {}
   provider_name     = "ADFS"
   user_pool_id      = aws_cognito_user_pool.default.id
-  metadata_url      = "https://s3-hosted-adfs-xml.com"
+  metadata_url      = "https://dev-4181175.okta.com/app/exk1lkocfKhH8ytxx5d6/sso/saml/metadata"
 
 }
 
