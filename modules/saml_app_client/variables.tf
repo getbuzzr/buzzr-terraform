678082c243e6554d574925600d6ab98673aeee4b
Brandon Lau <brandonlau2@gmail.com>
2020-12-07 20:53:29 -0800

    build: add test SAML callback URL
    #16

--- modules/saml_app_client/variables.tf
+++ modules/saml_app_client/variables.tf
@@ -1,29 +1,29 @@
 variable "user_pool_id" {
   type        = string
-  description = "(required)This is the id of the user pool"
+  description = "(Required) The id of the user pool."
 }
 
 variable "callback_urls" {
   type        = list(string)
-  description = "This is the list of callback urls allowed"
+  description = "(Optional) List of allowed callback URLs for the identity providers."
   default     = []
 }
 
 variable "default_redirect_uri" {
   type        = string
-  description = "This is the default reirect uri"
+  description =  "(Optional) The default redirect URI. Must be in the list of callback URLs."
   default     = ""
 }
 
 variable "logout_urls" {
   type        = list(string)
-  description = "This is a list of logout urls allowed"
+  description = "(Optional) List of allowed logout URLs for the identity providers."
   default     = []
 }
 
 variable "app_client_name" {
   type        = string
-  description = "This is the app client name"
+  description = "The app client name."
 }
 
 variable "supported_identity_providers" {
