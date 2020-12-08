ea747fa9224c10912ddfe4ff601296728861dab8
Dennis Lau <dennislzf@gmail.com>
2020-12-07 17:01:28 -0800

    added new nacl and assigned subnets

--- modules/nacl_tcp_rule/main.tf
+++ modules/nacl_tcp_rule/main.tf
@@ -1,7 +1,18 @@
-resource "aws_network_acl_rule" "default" {
+resource "aws_network_acl_rule" "default_egress" {
   network_acl_id = var.network_acl_id
   rule_number    = var.rule_number
   egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = var.cidr_block
+  from_port      = var.port_number
+  to_port        = var.port_number
+}
+
+resource "aws_network_acl_rule" "default_ingress" {
+  network_acl_id = var.network_acl_id
+  rule_number    = var.rule_number
+  egress         = false
   protocol       = "tcp"
   rule_action    = "allow"
   cidr_block     = var.cidr_block
