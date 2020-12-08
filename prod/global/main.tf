ce08807b2a5a1ec636b17295e4a2ba116f96d38f
Dennis Lau <dennislzf@gmail.com>
2020-12-07 21:00:08 -0800

    fixed

--- prod/global/main.tf
+++ prod/global/main.tf
@@ -5,11 +5,16 @@ terraform {
       version = "~> 2.70"
     }
   }
+  backend "s3" {
+    bucket = "onguard-staging-terraform-state"
+    key    = "us-east-1/terraform.state"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
   profile = "default"
-  
+
   assume_role {
     role_arn = "arn:aws:iam::995213493585:role/prod_admin"
   }
