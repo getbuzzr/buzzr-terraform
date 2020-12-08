2bb43f5bea820e7a116779f4d63ca0f3c46d549f
Dennis Lau <dennislzf@gmail.com>
2020-12-08 15:40:55 -0800

    fixed filepath

--- dev/us-east-1/lambda.tf
+++ dev/us-east-1/lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "cognito_presignup_trigger" {
   function_name = "cognito_presignup_trigger"
   role          = module.presignup_lambda_role.role_arn
   handler       = "lambda_function.lambda_handler"
-  filename      = "assets/lambda/empty.zip"
+  filename      = "../../assets/lambda/empty.zip"
 
 
   runtime = "python3.8"
