9b662c3caf5cc3133468d717bd93fa33437859f5
Dennis Lau <dennislzf@gmail.com>
2020-12-09 18:40:46 -0800

    changed state file

--- prod/global/main.tf
+++ prod/global/main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   backend "s3" {
     bucket = "onguard-prod-terraform-state"
-    key    = "us-east-1/terraform.state"
+    key    = "global/terraform.state"
     region = "us-east-1"
   }
 }
