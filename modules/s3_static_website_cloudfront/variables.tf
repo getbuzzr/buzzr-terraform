0c9d5487c9e86dce3cfbd6386b193bafc8f0f53c
Dennis Lau <dennislzf@gmail.com>
2020-12-11 10:01:54 -0800

    added s3 back

---
+++ modules/s3_static_website_cloudfront/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+description = "The aws region of bucket"
+}
+
+variable "domain_name" {
+    description = "The domain name"
+}
+
+variable "acm_certificate_arn"{
+    description = "The acm arn"
+}
