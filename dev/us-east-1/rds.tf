490cd8f2e5dad99c5860a3379e46ed168cedcb79
Dennis Lau <dennislzf@gmail.com>
2020-12-11 16:09:39 -0800

    changed name

--- dev/us-east-1/rds.tf
+++ dev/us-east-1/rds.tf
@@ -11,7 +11,7 @@ resource "aws_db_subnet_group" "db_subnet_group" {
 
 module "api_db_server" {
   source = "../../modules/serverless_aurora_rds"
-  db_name = "APIDBServer"
+  db_name = "onguardapi"
   db_subnet_group = aws_db_subnet_group.db_subnet_group.name
   db_password_ssm_path = "api_db_server_password"
   max_capacity_unit             = 1
