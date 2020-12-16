3f97b7aa83f8252248f1f46dc76262fad274af72
Dennis Lau <dennislzf@gmail.com>
2020-12-16 11:35:43 -0800

    changed dynamo features

--- dev/us-east-1/dynamodb.tf
+++ dev/us-east-1/dynamodb.tf
@@ -1,10 +1,17 @@
 resource "aws_dynamodb_table" "live-checkin-table" {
   name           = "live_checkin"
   billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "group_id"
+  hash_key       = "userId"
+  range_key = "groupId"
+  
+  attribute {
+    name = "groupId"
+    type = "N"
+  }
 
   attribute {
-    name = "group_id"
-    type = "S"
+    name = "userId"
+    type = "N"
   }
+  
 }
