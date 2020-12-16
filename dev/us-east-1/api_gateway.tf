fe1f70134e7722cb0537ec1b0fd78a86dee6c613
Dennis Lau <dennislzf@gmail.com>
2020-12-15 18:19:25 -0800

    remove role

--- dev/us-east-1/api_gateway.tf
+++ dev/us-east-1/api_gateway.tf
@@ -12,7 +12,9 @@ resource "aws_apigatewayv2_api" "checkin_api_gateway" {
 }
 resource "aws_apigatewayv2_route" "checkin_api_route" {
   api_id    = aws_apigatewayv2_api.checkin_api_gateway.id
-  route_key = "$default"
+  route_key = "POST /checkin"
+  authorization_type = "CUSTOM"
+  authorizer_id = aws_apigatewayv2_authorizer.checkin_gateway_authorizer.id
   target = "integrations/${aws_apigatewayv2_integration.checkin_api_gateway.id}"
 }
 resource "aws_apigatewayv2_integration" "checkin_api_gateway" {
@@ -40,6 +42,7 @@ resource "aws_apigatewayv2_authorizer" "checkin_gateway_authorizer" {
 resource "aws_apigatewayv2_stage" "checkin_stage" {
   api_id      = aws_apigatewayv2_api.checkin_api_gateway.id
   name        = "live"
+  auto_deploy = true
 }
 # # role to write to cloudwatch
 # resource "aws_api_gateway_account" "checkin_account" {
