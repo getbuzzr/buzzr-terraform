749c5dea2e491fd641b734b3c651e0eaa705fb49
Dennis Lau <dennislzf@gmail.com>
2020-12-15 16:06:15 -0800

    added lambda/dynamo/roles for checkin

---
+++ dev/us-east-1/role_expiry_trigger_lambda.tf
@@ -0,0 +1,91 @@
+
+# Role defined for expiry trigger
+data "aws_iam_policy_document" "expiry_trigger_lambda_policy" {
+
+  statement {
+    sid = "CheckinCloudwatchLogs"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+
+    resources = [
+      "arn:aws:logs:us-east-1:*:*"
+    ]
+  }
+
+  statement {
+    sid = "CheckinInvokePermissions"
+
+    actions = [
+      "lambda:InvokeFunction"
+    ]
+
+    resources = [
+      "arn:aws:lambda:us-east-1:*:function:expiry_trigger"
+    ]
+  }
+
+  statement {
+    sid = "ExpiryLiveCheckinReadWrite"
+
+    actions = [
+      "dynamodb:*"
+    ]
+
+    resources = [
+      "arn:aws:dynamodb:us-east-1:*:table/live_checkin"
+    ]
+  }
+  statement {
+     sid = "ExpirynGetAPIDbPassword"
+
+      actions = [
+        "ssm:GetParameter*",
+        "ssm:DescribeParameters"
+      ]
+
+      resources = [
+        "arn:aws:ssm:us-east-1:*:parameter/api_db_server_password"
+      ]
+  }
+  statement {
+     sid = "CheckinSQSEnqueue"
+
+      actions = [
+        "sqs:SendMessage",
+        "sqs:ReceiveMessage",
+        "sqs:DeleteMessage",
+        "sqs:GetQueueAttributes"
+      ]
+
+      resources = [
+        "arn:aws:sqs:us-east-1:*:checkin_queue"
+      ]
+  }
+
+}
+
+data "aws_iam_policy_document" "expiry_trigger_lambda_arp" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+
+      identifiers = [
+        "lambda.amazonaws.com"
+      ]
+    }
+  }
+}
+
+module "checkin_consumer_lambda_role" {
+  source             = "../../modules/generic_role"
+  role_name          = "expiry_trigger_lambda"
+  description        = "This is the role that the expiry trigger assumes"
+  assume_role_policy = data.aws_iam_policy_document.expiry_trigger_lambda_arp.json
+  policy_document    = data.aws_iam_policy_document.expiry_trigger_lambda_policy.json
+}
---
+++ dev/us-east-1/sqs.tf
@@ -0,0 +1,11 @@
+
+resource "aws_sqs_queue" "checkin_queue" {
+  name                      = "checkin_queue"
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+}
+
+resource "aws_lambda_event_sourece_mapping" "checkin_enqueue_trigger" {
+    event_source_arn = aws_sqs_queue.checkin_queue.arn
+    function_name =  module.expiry_trigger.arn
+}
