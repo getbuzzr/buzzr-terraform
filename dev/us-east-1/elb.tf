43ff53e8e029bc9ca7845544830872ac724828fc
Dennis Lau <dennislzf@gmail.com>
2020-12-21 12:39:45 -0800

    var change

--- dev/us-east-1/elb.tf
+++ dev/us-east-1/elb.tf
@@ -34,7 +34,7 @@ resource "aws_elastic_beanstalk_environment" "onguard_dev_env" {
   setting {
     namespace = "aws:ec2:vpc"
     name      = "Subnets"
-    value     = [aws_subnet.public1.id,aws_subnet.public2.id,aws_subnet.private1.id,aws_subnet.private2.id]
+    value     = "${aws_subnet.public1.id},${aws_subnet.public2.id},${aws_subnet.private1.id},${aws_subnet.private2.id}"
   }
 
   setting {
