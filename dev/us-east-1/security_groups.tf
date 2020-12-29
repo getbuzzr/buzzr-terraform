d277f8f50576b9c79461df7e0f7cc990633b1655
Dennis Lau <dennislzf@gmail.com>
2020-12-29 15:48:23 -0800

    Added db security group

--- dev/us-east-1/security_groups.tf
+++ dev/us-east-1/security_groups.tf
@@ -52,3 +52,17 @@ resource "aws_security_group" "web_server" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "db_server" {
+  name        = "dbserver"
+  description = "Allow db traffic from webserver"
+  vpc_id      = aws_default_vpc.default.id
+  ingress {
+    description = "SQL from webserver "
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.web_server.id]
+
+  }
+}
