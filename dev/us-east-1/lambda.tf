825aa34be1514ed8a7e639906627a39441437f50
Dennis Lau <dennislzf@gmail.com>
2020-12-31 12:50:18 -0800

    added postsignup lambda

--- dev/us-east-1/lambda.tf
+++ dev/us-east-1/lambda.tf
@@ -42,3 +42,24 @@ module "expiry_trigger" {
   runtime = "python3.8"
 
 }
+
+
+module "cognito_postsignup_trigger" {
+  source = "../../modules/lambda_function"
+
+  function_name = "cognito_postsignup_trigger"
+  role_arn      = module.presignup_lambda_role.role_arn
+  handler       = "main.lambda_handler"
+
+
+  runtime = "python3.8"
+
+}
+
+resource "aws_lambda_permission" "cognito_postsignup_permission" {
+  statement_id  = "AllowExecutionCognito"
+  action        = "lambda:InvokeFunction"
+  function_name = module.cognito_postsignup_trigger.arn
+  principal     = "cognito-idp.amazonaws.com"
+  source_arn    = aws_cognito_user_pool.default.arn
+}
