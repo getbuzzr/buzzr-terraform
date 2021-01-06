4df5b3e70d99463836fd91615991793151a3e22d
Dennis Lau <dennislzf@gmail.com>
2021-01-06 11:01:39 -0800

    set default

--- modules/lambda_function/variables.tf
+++ modules/lambda_function/variables.tf
@@ -17,9 +17,11 @@ variable "runtime" {
 
 variable "vpc_subnet_ids"{
   description = "(list) subnet ids"
+  default     = null
 }
 
 variable "vpc_security_group_ids"{
   description = "(list) vpc security group= ids"
+  default     = null
 }
 
