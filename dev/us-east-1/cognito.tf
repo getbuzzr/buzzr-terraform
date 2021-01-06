356d78d90912400e74b8dd3c662651d738aec7f6
Dennis Lau <dennislzf@gmail.com>
2021-01-05 16:05:12 -0800

    changed to cognito to use local

--- dev/us-east-1/cognito.tf
+++ dev/us-east-1/cognito.tf
@@ -9,6 +9,7 @@ data "aws_ssm_parameter" "apple_client_private_key" {
 locals {
   okta_idp_provider_name = "Okta"
   adfs_idp_provider_name = "ADFS"
+  cognito_idp_provider_name = "COGNITO"
 }
 
 resource "aws_cognito_user_pool" "default" {
@@ -43,9 +44,9 @@ module "mobile_app_client" {
   supported_identity_providers = [
     local.adfs_idp_provider_name,
     local.okta_idp_provider_name,
+    local.cognito_idp_provider_name,
     aws_cognito_identity_provider.google.provider_name,
-    aws_cognito_identity_provider.apple.provider_name,
-    "COGNITO"
+    aws_cognito_identity_provider.apple.provider_name
   ]
   default_redirect_uri = "https://oauth.onguard.co/"
   # add callback urls here
@@ -67,9 +68,9 @@ module "web_app_client" {
   supported_identity_providers = [
     local.adfs_idp_provider_name,
     local.okta_idp_provider_name,
+    local.cognito_idp_provider_name,
     aws_cognito_identity_provider.google.provider_name,
-    aws_cognito_identity_provider.apple.provider_name,
-    "COGNITO"
+    aws_cognito_identity_provider.apple.provider_name
   ]
   #default_redirect_uri
   default_redirect_uri = ""
