9140d3b41ca9c05817276f4ee590c41767781b9c
Dennis Lau <dennislzf@gmail.com>
2021-01-06 10:35:35 -0800

    changed conditional to ternary

--- modules/lambda_function/main.tf
+++ modules/lambda_function/main.tf
@@ -4,12 +4,12 @@ resource "aws_lambda_function" "default" {
   handler       = var.handler
   filename      = "${path.module}/files/empty.zip"
   # if subnets present then vpc is also
-  if var.subnets {
+  var.subnets? {
     vpc_config {
       subnet_ids         = var.subnets
       security_group_ids = var.security_groups
     } 
-  }
+  }:""
 
   runtime = var.runtime
 
