8cb1233266a9bc6a397972f263c790945e932623
Dennis Lau <dennislzf@gmail.com>
2021-01-08 12:16:54 -0800

    added dev.api domain

--- dev/us-east-1/acm.tf
+++ dev/us-east-1/acm.tf
@@ -6,3 +6,12 @@ resource "aws_acm_certificate" "dev_auth_onguard_co" {
     create_before_destroy = true
   }
 }
+
+resource "aws_acm_certificate" "dev_api_onguard_co" {
+  domain_name       = "dev.api.onguard.co"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
