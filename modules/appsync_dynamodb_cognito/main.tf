60575dfd35640c78bdc5dba62b367a4a74651934
Brandon Lau <brandonlau2@gmail.com>
2021-01-20 18:36:52 -0800

    feat: add additional API key authentication method to allow clients to donwnload GraphQL schema
    #90

--- modules/appsync_dynamodb_cognito/main.tf
+++ modules/appsync_dynamodb_cognito/main.tf
@@ -12,6 +12,16 @@ resource "aws_appsync_graphql_api" "default" {
     default_action = "ALLOW"
     user_pool_id   = var.cognito_user_pool_id
   }
+
+  additional_authentication_provider {
+    authentication_type = "API_KEY"
+  }
+}
+
+resource "aws_appsync_api_key" "default" {
+  api_id      = aws_appsync_graphql_api.default.id
+  description = "API key to allow mobile clients to download the GraphQL schema via introspection."
+  expires     = "2022-01-01T00:00:00Z"
 }
 
 resource "aws_appsync_datasource" "dynamodb" {
