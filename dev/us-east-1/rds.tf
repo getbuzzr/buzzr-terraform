d91c674721365a6fbb2d46e02c4786856b19da36
Dennis Lau <dennislau@Denniss-MBP.hitronhub.home>
2021-03-31 13:54:55 -0700

    added fix

--- dev/us-east-1/rds.tf
+++ dev/us-east-1/rds.tf
@@ -1,13 +1,20 @@
 
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "db"
+  subnet_ids = [aws_subnet.private3.id, aws_subnet.private4.id]
+
+  tags = {
+    Name = "Private DB subnet"
+  }
+}
 resource "aws_db_subnet_group" "elb_subnet_group" {
   name       = "db"
   subnet_ids = [aws_subnet.public1.id, aws_subnet.public2.id]
 
   tags = {
-    Name = "Public DB subnet"
+    Name = "public DB subnet"
   }
 }
-
 
 module "api_db_server" {
   source               = "../../modules/serverless_aurora_rds"
