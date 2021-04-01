827e16bd56f15073fa21601a7d2a6b378202f19c
Dennis Lau <dennislau@Denniss-MBP.hitronhub.home>
2021-04-01 14:36:24 -0700

    adad

--- dev/us-east-1/lambda.tf
+++ dev/us-east-1/lambda.tf
@@ -34,8 +34,9 @@ module "cognito_postsignup_trigger" {
   lambda_layer_arns = [aws_lambda_layer_version.pymysql_layer.arn]
   runtime = "python3.8"
   timeout = 30
+  # dont use in prod.. use ssm
   variables     = {
-                      DATABASE_URI = data.aws_ssm_parameter.api_db_server.value
+                      DATABASE_URI = data.aws_ssm_parameter.api_db_server_uri.value
                     }
 
 
