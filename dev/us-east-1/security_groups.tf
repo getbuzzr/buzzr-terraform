f4c681f09baea8efefcbd711fbc810cdc709beba
Dennis Lau <dennislau@Denniss-MBP.hitronhub.home>
2021-04-01 14:05:37 -0700

    added fix

--- dev/us-east-1/security_groups.tf
+++ dev/us-east-1/security_groups.tf
@@ -31,18 +31,12 @@ resource "aws_security_group" "lambda" {
   vpc_id      = aws_default_vpc.default.id
   ingress {
     description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress {
-    description = "http from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+ 
 
   egress {
     from_port   = 0
