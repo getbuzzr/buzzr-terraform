c04510e9c085be8e11a2dbbd265e0770f9f4d37d
Dennis Lau <dennislau@Denniss-MBP.hitronhub.home>
2021-04-09 10:54:50 -0700

    remove dupe

--- prod/us-east-1/acm.tf
+++ prod/us-east-1/acm.tf
@@ -15,13 +15,3 @@ resource "aws_acm_certificate" "auth_getbuzzr_co" {
     create_before_destroy = true
   }
 }
-
-
-resource "aws_acm_certificate" "oauth_getbuzzr_co" {
-  domain_name       = "oauth.getbuzzr.co"
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
