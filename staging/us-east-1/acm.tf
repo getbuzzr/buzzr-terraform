19dacac9f3ff40ab23950e1f02d2fc0a0376a2ca
Dennis Lau <dennislau@Denniss-MBP.hitronhub.home>
2021-05-10 13:31:13 -0700

    added  certs

---
+++ staging/us-east-1/acm.tf
@@ -0,0 +1,35 @@
+resource "aws_acm_certificate" "dev_auth_getbuzzr_co" {
+  domain_name       = "dev.auth.getbuzzr.co"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate" "dev_api_getbuzzr_co" {
+  domain_name       = "dev.api.getbuzzr.co"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate" "dev_admin_getbuzzr_co" {
+  domain_name       = "dev.admin.getbuzzr.co"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate" "dev_static_getbuzzr_co" {
+  domain_name       = "dev.static.getbuzzr.co"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
