3f95af1200b293f28f21cbd14c52e80517c10d38
Dennis Lau <dennislau@Denniss-MBP.hitronhub.home>
2021-05-17 16:54:23 -0700

    added fix

--- root/global/route53.tf
+++ root/global/route53.tf
@@ -109,7 +109,7 @@ resource "aws_route53_record" "dev_auth_getbuzzr_co" {
   type    = "A"
 
   alias {
-    name                   = "dlpdmbxc8jqgb.cloudfront.net"
+    name                   = "d28bj650amctzt.cloudfront.net"
     zone_id                = local.cloudfront_distribution_hosted_zone_id
     evaluate_target_health = true
   }
