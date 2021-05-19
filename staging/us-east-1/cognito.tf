586741487c9d6aab37a46fc00e84b1279df86cca
Dennis Lau <dennislau@Denniss-MBP.hitronhub.home>
2021-05-19 11:46:33 -0700

    fixed user pool

--- staging/us-east-1/cognito.tf
+++ staging/us-east-1/cognito.tf
@@ -13,7 +13,7 @@ locals {
   cognito_idp_provider_name = "COGNITO"
 }
 
-resource "aws_cognito_user_pool" "staging_user_pool" {
+resource "aws_cognito_user_pool" "user_pool" {
   name                     = "getbuzzr-user-pool"
   auto_verified_attributes = ["email"]
   username_attributes      = ["email"]
@@ -48,14 +48,14 @@ resource "aws_cognito_user_pool" "staging_user_pool" {
   }
 }
 resource "aws_cognito_user_pool_domain" "default" {
-  user_pool_id    = aws_cognito_user_pool.staging_user_pool.id
+  user_pool_id    = aws_cognito_user_pool.user_pool.id
   domain          = "staging.auth.getbuzzr.co"
   certificate_arn = aws_acm_certificate.staging_auth_getbuzzr_co.arn
 }
 
 module "mobile_app_client" {
   source          = "../../modules/saml_app_client"
-  user_pool_id    = aws_cognito_user_pool.staging_user_pool.id
+  user_pool_id    = aws_cognito_user_pool.user_pool.id
   app_client_name = "mobile_app_client"
   supported_identity_providers = [
     local.cognito_idp_provider_name,
@@ -74,7 +74,7 @@ module "mobile_app_client" {
 
 
 resource "aws_cognito_identity_provider" "google" {
-  user_pool_id  = aws_cognito_user_pool.staging_user_pool.id
+  user_pool_id  = aws_cognito_user_pool.user_pool.id
   provider_name = "Google"
   provider_type = "Google"
 
@@ -95,7 +95,7 @@ resource "aws_cognito_identity_provider" "google" {
 }
 
 resource "aws_cognito_identity_provider" "facebook" {
-  user_pool_id  = aws_cognito_user_pool.staging_user_pool.id
+  user_pool_id  = aws_cognito_user_pool.user_pool.id
   provider_name = "Facebook"
   provider_type = "Facebook"
 
@@ -116,7 +116,7 @@ resource "aws_cognito_identity_provider" "facebook" {
 }
 
 resource "aws_cognito_identity_provider" "apple" {
-  user_pool_id  = aws_cognito_user_pool.staging_user_pool.id
+  user_pool_id  = aws_cognito_user_pool.user_pool.id
   provider_name = "SignInWithApple"
   provider_type = "SignInWithApple"
 
