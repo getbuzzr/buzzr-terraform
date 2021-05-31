cf826b101bb5bb6ba28e45d35a91fcc47f39d1a1
Dennis Lau <dennislau@Denniss-MBP.hitronhub.home>
2021-05-31 15:44:39 -0700

    added

--- prod/us-east-1/rds.tf
+++ prod/us-east-1/rds.tf
@@ -12,35 +12,34 @@ resource "aws_db_subnet_group" "db_subnet_group" {
   }
 }
 
-# module "buzzr_prod_db" {
-#   source  = "terraform-aws-modules/rds-aurora/aws"
-#   version = "5.2.0"
+module "buzzr_prod_db" {
+  source  = "terraform-aws-modules/rds-aurora/aws"
 
-#   name           = "buzzr"
-#   engine         = "aurora-mysql"
-#   engine_version = "5.7"
-#   instance_type  = "db.t3.small"
+  name           = "buzzr"
+  engine         = "aurora-mysql"
+  engine_version = "5.7"
+  instance_type  = "db.t3.small"
 
-#   vpc_id  = aws_default_vpc.default.id
-#   subnets = [aws_subnet.private_db1.id, aws_subnet.private_db2.id]
+  vpc_id  = aws_default_vpc.default.id
+  subnets = [aws_subnet.private_db1.id, aws_subnet.private_db2.id]
 
-#   replica_count           = 1
-#   allowed_security_groups = [aws_security_group.db_server.id]
-#   allowed_cidr_blocks     = ["172.31.4.0/24","172.31.5.0/24"]
-#   database_name                   = "buzzr"
-#   username               = "root"
-#   password               = data.aws_ssm_parameter.db_server_password.value
-#   storage_encrypted   = true
-#   apply_immediately   = true
-#   monitoring_interval = 10
+  replica_count           = 1
+  allowed_security_groups = [aws_security_group.db_server.id]
+  allowed_cidr_blocks     = ["172.31.4.0/24","172.31.5.0/24"]
+  database_name                   = "buzzr"
+  username               = "root"
+  password               = data.aws_ssm_parameter.db_server_password.value
+  storage_encrypted   = true
+  apply_immediately   = true
+  monitoring_interval = 10
 
-#   db_parameter_group_name         = "default"
-#   db_cluster_parameter_group_name = "default"
+  db_parameter_group_name         = "default"
+  db_cluster_parameter_group_name = "default"
 
-#   enabled_cloudwatch_logs_exports = ["mysql"]
+  enabled_cloudwatch_logs_exports = ["mysql"]
 
-#   tags = {
-#     Environment = "prod"
-#     Terraform   = "true"
-#   }
-# }
+  tags = {
+    Environment = "prod"
+    Terraform   = "true"
+  }
+}
