da7c443f8f3638f5a55c53b4bfb909e278404cd2
Dennis Lau <dennislau@Denniss-MBP.hitronhub.home>
2021-05-31 16:52:59 -0700

    added security

---
+++ prod/us-east-1/security_groups.tf
@@ -0,0 +1,75 @@
+resource "aws_security_group" "load_balancer" {
+  name        = "load_balancer"
+  description = "Allow Internet Traffic"
+  vpc_id      = aws_default_vpc.default.id
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "web_server" {
+  name        = "webservers"
+  description = "Allow Internet Traffic from ALB"
+  vpc_id      = aws_default_vpc.default.id
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    security_groups = [aws_security_group.load_balancer.id]
+
+  }
+  ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.load_balancer.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "db_server" {
+  name        = "dbserver"
+  description = "Allow db traffic from webserver"
+  vpc_id      = aws_default_vpc.default.id
+  ingress {
+    description = "SQL from webserver "
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.web_server.id]
+  }
+
+  egress {
+    description = "SQL from webserver "
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    security_groups = [aws_security_group.web_server.id]
+  }
+}
