e282cee081c0b22a66ea7cf6d28a6f1991dc0a12
Dennis Lau <dennislau@Denniss-MBP.hitronhub.home>
2021-05-31 14:46:26 -0700

    added auth/added nat gateway

--- prod/us-east-1/acm.tf
+++ prod/us-east-1/acm.tf
@@ -7,14 +7,14 @@ resource "aws_acm_certificate" "oauth_getbuzzr_co" {
   }
 }
 
-# resource "aws_acm_certificate" "auth_getbuzzr_co" {
-#   domain_name       = "auth.getbuzzr.co"
-#   validation_method = "DNS"
+resource "aws_acm_certificate" "auth_getbuzzr_co" {
+  domain_name       = "auth.getbuzzr.co"
+  validation_method = "DNS"
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 resource "aws_acm_certificate" "dev_api_getbuzzr_co" {
   domain_name       = "api.getbuzzr.co"
