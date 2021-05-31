6ae6446131057ae783f4d2344e83d3b54210164b
Dennis Lau <dennislau@Denniss-MBP.hitronhub.home>
2021-05-31 16:35:58 -0700

    create

--- prod/us-east-1/rds.tf
+++ prod/us-east-1/rds.tf
@@ -13,7 +13,7 @@ resource "aws_db_subnet_group" "db_subnet_group" {
 }
 # create IAM role for monitoring
 resource "aws_iam_role" "enhanced_monitoring" {
-  name               = "rds-cluster-example-1"
+  name               = "enhanced-monitoring"
   assume_role_policy = data.aws_iam_policy_document.enhanced_monitoring.json
 }
 
@@ -71,27 +71,54 @@ data "aws_iam_policy_document" "enhanced_monitoring" {
 #   }
 # }
 
-module "buzzr_prod_db" {
-  source = "cloudposse/rds-cluster/aws"
-  # Cloud Posse recommends pinning every module to a specific version
-  version     = "0.45.0"
-  engine          = "aurora"
-  cluster_family  = "aurora-mysql5.7"
-  cluster_size    = 2
-  namespace       = "bz"
-  stage           = "prod"
-  name            = "buzzr"
-  admin_user      = "root"
-  admin_password  = data.aws_ssm_parameter.db_server_password.value
-  db_name         = "buzzr"
-  instance_type   = "db.t2.small"
-  vpc_id          = aws_default_vpc.default.id
-  security_groups = [aws_security_group.db_server.id]
-  subnets         = [aws_subnet.private_db1.id, aws_subnet.private_db2.id]
-  backup_window= "1:00-3:00"
-  # enable monitoring every 30 seconds
-  rds_monitoring_interval = 30
+# module "buzzr_prod_db" {
+#   source = "cloudposse/rds-cluster/aws"
+#   # Cloud Posse recommends pinning every module to a specific version
+#   version     = "0.45.0"
+#   engine          = "aurora"
+#   cluster_family  = "aurora-mysql5.7"
+#   cluster_size    = 2
+#   namespace       = "bz"
+#   stage           = "prod"
+#   name            = "buzzr"
+#   admin_user      = "root"
+#   admin_password  = data.aws_ssm_parameter.db_server_password.value
+#   db_name         = "buzzr"
+#   instance_type   = "db.t2.small"
+#   vpc_id          = aws_default_vpc.default.id
+#   security_groups = [aws_security_group.db_server.id]
+#   subnets         = [aws_subnet.private_db1.id, aws_subnet.private_db2.id]
+#   backup_window= "1:00-3:00"
+#   # enable monitoring every 30 seconds
+#   rds_monitoring_interval = 30
 
-  # reference iam role created above
-  rds_monitoring_role_arn = aws_iam_role.enhanced_monitoring.arn
+#   # reference iam role created above
+#   rds_monitoring_role_arn = aws_iam_role.enhanced_monitoring.arn
+# }
+
+resource "aws_rds_cluster_instance" "cluster_instances" {
+  count              = 2
+  identifier         = "buzzr-api-${count.index}"
+  cluster_identifier = aws_rds_cluster.default.id
+  instance_class     = "db.t3.small"
+  engine             = buzzr_api_cluster.default.engine
+  engine_version     = buzzr_api_cluster.default.engine_version
+  publicly_accessible=true
+  db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
+  monitoring_role_arn = aws_iam_role.enhanced_monitoring.arn
+  preferred_backup_window = "02:00-05:00"
+}
+
+resource "aws_rds_cluster" "buzzr_api_cluster" {
+  cluster_identifier      = "buzzr-api"
+  engine                  = "aurora-mysql"
+  engine_version          = "5.7.mysql_aurora.2.03.2"
+  availability_zones      = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  database_name           = "buzzr"
+  master_username         = "root"
+  master_password         = data.aws_ssm_parameter.db_server_password.value
+  backup_retention_period = 5
+  preferred_backup_window = "02:00-05:00"
+  apply_immediately=true
+  db_subnet_group_name =aws_db_subnet_group.db_subnet_group.name
 }
