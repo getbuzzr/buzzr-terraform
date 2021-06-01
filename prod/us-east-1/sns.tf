c64f2c208a06c2765cc2e22072dc0501173eacfe
Dennis Lau <dennislau@Denniss-MBP.hitronhub.home>
2021-05-31 21:09:43 -0700

    added sns

---
+++ prod/us-east-1/sns.tf
@@ -0,0 +1,14 @@
+resource "aws_sns_platform_application" "gcm_application" {
+  name                = "gcm_application"
+  platform            = "GCM"
+  platform_credential = aws_ssm_parameter.gcm_api_key.value
+}
+
+resource "aws_sns_platform_application" "apns_application" {
+  name = "apns_application"
+  # user "APNS" for prod
+  platform                     = "APNS"
+  platform_credential          = aws_ssm_parameter.apns_private_key.value
+  platform_principal           = aws_ssm_parameter.apns_certificate.value
+  success_feedback_sample_rate = 100
+}
