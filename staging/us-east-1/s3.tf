bf9cfec4dea2ab4a69c338b555f689f47385aaf5
Dennis Lau <dennislau@Denniss-MacBook-Pro.local>
2021-06-13 12:09:12 -0700

    added oauth call backs

--- staging/us-east-1/s3.tf
+++ staging/us-east-1/s3.tf
@@ -3,3 +3,11 @@ module "elb_deploy_bucket" {
   bucket_name   = "buzzr-elb-deploy-stage"
   read_role_arn = module.elb_webserver_role.role_arn
 }
+
+module "staging_oauth_getbuzzr_co" {
+  source              = "../../modules/s3_static_website_cloudfront"
+  aws_region          = "us-east-1"
+  domain_name         = "staging.oauth.getbuzzr.co"
+  acm_certificate_arn = aws_acm_certificate.staging_oauth_getbuzzr_co.arn
+  admin_role_arn      = "arn:aws:iam::096377267832:role/cicd_role"
+}
