59e63737c94a2384a15a5b6dacfcb5385b01f905
Dennis Lau <dennislau@Denniss-MacBook-Pro.local>
2021-08-03 20:44:27 -0700

    remove staging

--- staging/us-east-1/rds.tf
+++ staging/us-east-1/rds.tf
@@ -1,40 +1,40 @@
 
-data "aws_ssm_parameter" "db_server_password" {
-  name = aws_ssm_parameter.api_db_server_password.name
-}
+# data "aws_ssm_parameter" "db_server_password" {
+#   name = aws_ssm_parameter.api_db_server_password.name
+# }
 
-resource "aws_db_subnet_group" "db_subnet_group" {
-  name       = "db"
-  subnet_ids = [aws_subnet.private3.id, aws_subnet.private4.id]
+# resource "aws_db_subnet_group" "db_subnet_group" {
+#   name       = "db"
+#   subnet_ids = [aws_subnet.private3.id, aws_subnet.private4.id]
 
-  tags = {
-    Name = "Private DB subnet"
-  }
-}
-resource "aws_db_subnet_group" "elb_subnet_group" {
-  name       = "db_public"
-  subnet_ids = [aws_subnet.public1.id, aws_subnet.public2.id]
+#   tags = {
+#     Name = "Private DB subnet"
+#   }
+# }
+# resource "aws_db_subnet_group" "elb_subnet_group" {
+#   name       = "db_public"
+#   subnet_ids = [aws_subnet.public1.id, aws_subnet.public2.id]
 
-  tags = {
-    Name = "public DB subnet"
-  }
-}
+#   tags = {
+#     Name = "public DB subnet"
+#   }
+# }
 
 
 
-resource "aws_db_instance" "buzzr_staging_db" {
-  allocated_storage      = 10
-  identifier             = "buzzr"
-  engine                 = "mysql"
-  engine_version         = "5.7"
-  instance_class         = "db.t3.micro"
-  name                   = "buzzr"
-  username               = "root"
-  password               = data.aws_ssm_parameter.db_server_password.value
-  parameter_group_name   = "default.mysql5.7"
-  skip_final_snapshot    = true
-  multi_az               = false
-  publicly_accessible    = true
-  db_subnet_group_name   = aws_db_subnet_group.elb_subnet_group.name
-  vpc_security_group_ids = [aws_security_group.public_access.id]
-}
+# resource "aws_db_instance" "buzzr_staging_db" {
+#   allocated_storage      = 10
+#   identifier             = "buzzr"
+#   engine                 = "mysql"
+#   engine_version         = "5.7"
+#   instance_class         = "db.t3.micro"
+#   name                   = "buzzr"
+#   username               = "root"
+#   password               = data.aws_ssm_parameter.db_server_password.value
+#   parameter_group_name   = "default.mysql5.7"
+#   skip_final_snapshot    = true
+#   multi_az               = false
+#   publicly_accessible    = true
+#   db_subnet_group_name   = aws_db_subnet_group.elb_subnet_group.name
+#   vpc_security_group_ids = [aws_security_group.public_access.id]
+# }
